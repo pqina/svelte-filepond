84a317973910230d7adb0dfe4398bd7edca283e6
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2022-12-17 17:14:24 +0100

    Add types for svelte-filepond

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import type { FilePondOptions } from "filepond";
+import type { SvelteComponentTyped } from "svelte";
+
+export * from "filepond";
+
+export default class FilePond extends SvelteComponentTyped<FilePondOptions> {}
